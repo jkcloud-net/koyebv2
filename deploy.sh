278c6787cddcb3af6b7b2a57ac44c73a15b645de
yx10010 <59787094+yx10010@users.noreply.github.com>
2022-02-17 14:39:06 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,11 @@
 
 #UUID="c5f8a155-1a37-4394-949e-bdc2f586590b"
 #APP_NAME="www"
+
 REGION="hkg"
 
 if ! command -v flyctl >/dev/null 2>&1; then
-    printf '\e[33mCould not resolve command - flyctl. So, install flyctl first.\n\e[0m'
+    printf '\e[33mCould not resolve command - koyebctl. So, install koyeb first.\n\e[0m'
     curl -L https://raw.githubusercontent.com/koyeb/koyeb-cli/master/install.sh | sh | KOYEBCTL_INSTALL=/usr/local sh
 fi
 
