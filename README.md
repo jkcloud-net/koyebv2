797b70114ca27dc05c88503b1831f79127327235
yx10010 <59787094+yx10010@users.noreply.github.com>
2022-02-17 14:28:00 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,58 @@
+# V2Ray4Fly.io
+
+## 概述
+通过 GitHub Actions 自动在 [Fly.io](https://fly.io/) 上部署 [V2Ray](https://www.v2fly.org/)
+
+## 配置部署
+1. 先到 [Fly.io](https://fly.io/) 注册账号，***注意：注册时要记得绑定信用卡，银联的就行***
+2. GitHub Actions 增加`FLY_API_TOKEN`、`APP_NAME`和`UUID`三个安全字段
+* FLY_API_TOKEN - Fly API 接口 Token 值，可访问 <https://web.fly.io/user/personal_access_tokens> 或在本地执行`flyctl auth token`查看
+* APP_NAME - 应用名称，注意此名称全局唯一
+* UUID - 供用户连接的 UUID，可通过 <http://www.uuid.online/> 或 <https://www.uuidgenerator.net/> 生成
+3. 推送代码即可触发部署，另外已设置每月一号四点（UTC）自动部署
+
+## 客户端连接
+* 协议（protocol） - vmess
+* 地址（address） - [APP_NAME].fly.dev
+* 端口（port） - 443
+* 用户ID（id） - [UUID]
+* 额外ID（alterId） - 0 （！！！）
+* 传输协议（network） - ws
+* 底层传输安全（tls) - tls
+
+```json
+{
+  "inbounds": [],
+  "outbounds": [
+    {
+      "tag": "proxy",
+      "protocol": "vmess",
+      "settings": {
+        "vnext": [
+          {
+            "address": "[APP_NAME].fly.dev",
+            "port": 443,
+            "users": [
+              {
+                "id": "[UUID]",
+                "alterId": 0,
+                "security": "auto"
+              }
+            ]
+          }
+        ]
+      },
+      "streamSettings": {
+        "network": "ws",
+        "security": "tls",
+        "tlsSettings": {
+          "allowInsecure": true
+        }
+      }
+    }
+  ]
+}
+```
+
+## 参考
+* [V2Ray4Heroku](https://github.com/lyz7805/v2ray4heroku)
---
+++ deploy.sh
@@ -0,0 +1,78 @@
+#!/bin/sh
+
+#UUID="c5f8a155-1a37-4394-949e-bdc2f586590b"
+#APP_NAME="www"
+REGION="hkg"
+
+if ! command -v flyctl >/dev/null 2>&1; then
+    printf '\e[33mCould not resolve command - flyctl. So, install flyctl first.\n\e[0m'
+    curl -L https://raw.githubusercontent.com/koyeb/koyeb-cli/master/install.sh | sh | KOYEBCTL_INSTALL=/usr/local sh
+fi
+
+if [ -z "${APP_NAME}" ]; then
+    printf '\e[31mPlease set APP_NAME first.\n\e[0m' && exit 1
+fi
+
+flyctl info --app "${APP_NAME}" >/tmp/${APP_NAME} 2>&1;
+if [ "$(cat /tmp/${APP_NAME} | grep -o "Could not resolve App")" = "Could not resolve App" ]; then
+    printf '\e[33mCould not resolve app. Next, create the App.\n\e[0m'
+    flyctl apps create "${APP_NAME}" >/dev/null 2>&1;
+
+    flyctl info --app "${APP_NAME}" >/tmp/${APP_NAME} 2>&1;
+    if [ "$(cat /tmp/${APP_NAME} | grep -o "Could not resolve App")" != "Could not resolve App" ]; then
+        printf '\e[32mCreate app success.\n\e[0m'
+    else
+        printf '\e[31mCreate app failed.\n\e[0m' && exit 1
+    fi
+else
+    printf '\e[33mThe app has been created.\n\e[0m'
+fi
+
+printf '\e[33mNext, create app config file - fly.toml.\n\e[0m'
+cat <<EOF >./fly.toml
+app = "$APP_NAME"
+
+kill_signal = "SIGINT"
+kill_timeout = 5
+processes = []
+
+[env]
+
+[experimental]
+  allowed_public_ports = []
+  auto_rollback = true
+
+[[services]]
+  http_checks = []
+  internal_port = 443
+  # processes = ["app"]
+  protocol = "tcp"
+  script_checks = []
+
+  [services.concurrency]
+    hard_limit = 50
+    soft_limit = 35
+    type = "connections"
+
+  [[services.ports]]
+    handlers = ["http"]
+    port = 80
+
+  [[services.ports]]
+    handlers = ["tls", "http"]
+    port = 443
+
+  [[services.tcp_checks]]
+    grace_period = "120s"
+    interval = "15s"
+    restart_limit = 0
+    timeout = "2s"
+EOF
+printf '\e[32mCreate app config file success.\n\e[0m'
+printf '\e[33mNext, set app secrets and regions.\n\e[0m'
+
+flyctl secrets set UUID="${UUID}"
+flyctl regions set ${REGION}
+printf '\e[32mApp secrets and regions set success. Next, deploy the app.\n\e[0m'
+flyctl deploy --detach
+# flyctl status --app ${APP_NAME}
